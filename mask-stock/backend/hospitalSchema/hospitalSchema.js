ce89ab97c1055896f1f294982e1bcbdab73d3115
Stefan <54683170+ZentexGG@users.noreply.github.com>
2023-02-06 21:51:25 +0200

    Delete mask-stock directory

--- mask-stock/backend/hospitalSchema/hospitalSchema.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose")
-
-const hospitalSchema=new mongoose.Schema({
-    name: String,
-})
-
-module.exports=mongoose.model("Hospitals", hospitalSchema)
--- mask-stock/src/pages/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import HomePage from '../../components/HomePage/HomePage'
-
-export default function Home() {
-  return (
-    <HomePage />
-  )
-}

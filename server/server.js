7c659e984185b5097cfbca41629d72e043e9e0ac
Tanase Stefan <stefan.tanase2121@gmail.com>
2023-02-07 11:26:50 +0200

    automatic restock of masks on server startup if on first day of the month

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const Stock = require("./schemas/stock.model");
+
 app.use(express.json());
 const PORT = 8008;
 
@@ -14,14 +16,36 @@ const connectToDB = (dbName) => {
     .then((r) => console.log(`Connected to DB: ${r.connection.name}`));
 };
 
+const restockMasks = async () => {
+  const dateObj = new Date();
+  const today = dateObj.getDate();
+  if (today === 1) {
+    console.log("Restocking masks...");
+    await Stock.findOneAndUpdate(
+      {
+        _id: "63e203ea099b00a399695ca9",
+      },
+      {
+        stock: 10000
+      },
+      {
+        new: true,
+      }
+    );
+  } else {
+    console.log("Restocking masks not necessary");
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("All good chief.");
 });
 
 const apiRouter = require("./routes/api");
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.listen(PORT, async () => {
-    console.log(`Server on http://127.0.0.1:${PORT}`);
-    connectToDB('mask-stock');
-})
+  console.log(`Server on http://127.0.0.1:${PORT}`);
+  connectToDB("mask-stock");
+  restockMasks();
+});

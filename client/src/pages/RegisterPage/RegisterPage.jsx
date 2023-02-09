b29c2330cfefe6aa7ba8ae143e3f6a79fcbe9146
ZentexGG <stefan.tanase2121@gmail.com>
2023-02-09 23:49:58 +0200

    cleaned up registerpage and successpage

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,33 +1,36 @@
-import NavbarComponent from "../../components/NavbarComponent/NavbarComponent"
-import RegisterFormComponent from "../../components/RegisterFormComponent/RegisterFormComponent"
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
+import RegisterFormComponent from "../../components/RegisterFormComponent/RegisterFormComponent";
 import axios from "axios";
 import { useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-const RegisterPage=()=>{
-  const [name,setName]=useState("")
-  const navigate = useNavigate()
+const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
   const getCookie = async () => {
     try {
-      const {data} = await axios.get('http://localhost:8008/api/login', { withCredentials: true })
-      setName(data.message)  
-    }
-    catch (error) {
+      const { data } = await axios.get("http://localhost:8008/api/login", {
+        withCredentials: true,
+      });
+      setName(data.message);
+    } catch (error) {
       console.log(error);
     }
-  
-  }
-  useLayoutEffect(()=>{
-    getCookie()
-    console.log(name)
-  },[])
+  };
+  useLayoutEffect(() => {
+    getCookie();
+    console.log(name);
+  }, []);
 
   return (
-        !name&&<>
-        <NavbarComponent/>
-        <RegisterFormComponent/>
-        </> ||navigate("/main")
-  )
-}
+    (!name && (
+      <>
+        <NavbarComponent />
+        <RegisterFormComponent />
+      </>
+    )) ||
+    navigate("/main")
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
